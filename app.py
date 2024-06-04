4a09b6bd3e0c4de1472ee3c1c51505c1b3b856f7
vjanssens6060 <vjanssens@francoisdesales.be>
2024-06-04 09:55:27 +0200

    màj hmtl + app

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import session
 import csv
 import fn_classement_F1 as fnht
+import os
+import sqlite3
 
+def fn_get_db_name():
+    file_path = os.path.realpath(__file__)
+    work_dir = os.path.dirname(file_path)
+    db_name = f'{work_dir}/data.db'
+    return db_name
 
 app = Flask(__name__)
 app.secret_key = b'bafe004cc8de79cc96482b95db2d75473a3aa855b3270350267ccc92bddd46c5'
@@ -24,8 +31,78 @@ def encode_team():
         session['directeur'] = request.form['directeur']
         session['position_classement_constructeur'] = request.form['position_classement_constructeur']        
 
-        db_name = fnht.fn_get_db_name()                   
-        fnht.set_team_data(db_name, team_data)
+        db_name = fnht.fn_get_db_name() 
+        team_data = [session['nom'],session['directeur'],session['position_classement_constructeur']]
+        fnht.fn_set_team_data(db_name, team_data)
+        
+        return redirect('/index')
+
+def fn_read_db(db_name, table_name):
+    sqliteConnection = None
+    try:            
+        with sqlite3.connect(db_name, timeout=10) as sqliteConnection:
+            print(f"Connected to the database {db_name}")
+            cursor = sqliteConnection.cursor()                       
+            try:
+                if table_name == 'team':        
+                    cursor.execute(f"SELECT * FROM TEAM;")
+                    data = cursor.fetchall()
+                    print("SQLite script executed successfully")
+                    print(f'\nExecution du SELECT :')
+                    for line in data:
+                        print(line)
+                    print()
+            except sqlite3.Error as error:
+                print(f"Error while executing SQLite script: {error}")
+            finally:
+                cursor.close()
+    except sqlite3.Error as error:
+        print(f"Error while connecting to SQLite: {error}")
+    except Exception as error:
+        print(f"{error}")
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+            return data
+
+@app.route("/edit_team/<id>", methods=['GET', 'POST'])
+def edit_team(id):
+    if request.method == 'GET':
+        line = []
+        db_name = fn_get_db_name()
+        data = fn_read_db(db_name)
+
+        # Rechercher une ligne par son ID et modifier son contenu
+        for line in data:
+            if int(line['team_id']) == int(id):
+                nom = line['nom']
+                directeur =  line['directeur']
+                position_classement_constructeur = line['position_classement_constructeur']
+
+        return render_template('edit_team.html',
+                                nom = nom,
+                                directeur = directeur,
+                                position_classement_constructeur = position_classement_constructeur
+                                )
+     
+    elif request.method == 'POST':
+        session['nom'] = request.form['nom']
+        session['directeur'] = request.form['directeur']
+        session['position_classement_constructeur'] = request.form['position_classement_constructeur']
+        data = []
+
+        # Rechercher une ligne par son ID et modifier son contenu
+        for line in data:
+            if line['team_id'] == id:
+                line['nom'] = session['nom']
+                line['directeur'] = session['directeur']
+                line['position_classement_constructeur'] = session['position_classement_constructeur']
+
+        # Réécrire le fichier CSV avec les modifications
+        db_name = fnht.fn_get_db_name() 
+        team_data = [session['nom'],session['directeur'],session['position_classement_constructeur']]
+        fnht.fn_set_team_data(db_name, team_data)
         
         return redirect('/index')
 
